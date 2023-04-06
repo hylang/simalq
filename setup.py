10991c66d9d9ee33a38719580552ad5595e57667
Kodi Arfer <git@arfer.net>
2023-04-06 13:00:03 -0400

    Added `setup.py`.

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name = 'simalq',
+    version = '0.0.0',
+    author = 'Kodi B. Arfer',
+    description = 'Infinitesimal Quest 2 + ε: A turn-based puzzling dungeon crawler',
+    long_description = Path('README.rst').read_text(),
+    long_description_content_type = 'text/x-rst',
+    project_urls = {
+        'Source Code': 'https://github.com/hylang/simalq'},
+    install_requires = [
+        'hy @ git+https://github.com/hylang/hy@master',
+        'hyrule @ git+https://github.com/hylang/hyrule@master',
+        'toolz >= 0.12.0',
+        'construct >= 2.10.68'],
+    packages = setuptools.find_packages(),
+    package_data = dict(simalq = [
+        str(p.relative_to('simalq'))
+        for p in Path('simalq').rglob('*.hy')]),
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'])
